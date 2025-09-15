0ce51e3b8bde53299f6e242e9e790f21cb0d3608
moodykhali23 <b.wayag7@gmail.com>
2025-09-15 10:40:51 +0300

    added redis and bullmq for memory and queing

--- lib/middleware/project-auth.ts
+++ lib/middleware/project-auth.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { prisma } from '@/lib/db';
+import { prisma } from '@/lib/prisma';
 import { getCurrentUser } from '@/lib/auth';
 
 export async function validateProjectAccess(
